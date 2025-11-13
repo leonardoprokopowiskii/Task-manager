a4cf621ce559103ad63f24dc2cf3d0e601964759
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2025-11-12 23:37:17 -0300

    add main menu

---
+++ manager.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu do Gerenciador de tarefas:")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefa")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deletar tarefas completas")
+  print("6. Sair")
+
+  choice = input("Digite sua escolha: ")
+
+  if choice == "6":
+    break
+
+print("Programa finalizado!")
